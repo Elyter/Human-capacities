585dfbcfd9b1490b52f28b7d62457fbd394f7d37
Elyter <eliottbreg@gmail.com>
2025-01-08 15:07:55 +0100

    fix eslint

--- src/app/api/sequenceMemory/route.ts
+++ src/app/api/sequenceMemory/route.ts
@@ -22,7 +22,7 @@ async function readData() {
   }
 }
 
-async function writeData(data: any) {
+async function writeData(data: unknown) {
   await ensureFileExists();
   await fs.writeFile(dataFilePath, JSON.stringify(data, null, 2));
 }
